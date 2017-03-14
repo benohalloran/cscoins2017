2f913913a5b14a147d91065af22dde9bfedc4b02
Jack Valinsky <jvalinsk@u.rochester.edu>
2017-03-14 04:55:00 -0400

    got basic websocket client working, can get current challenge and hands it to solve

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,94 @@
 #include "solver.hpp"
-
+#include <uWS/uWS.h>
 #include <iostream>
 #include <cstdio>
+#include <string.h>
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
+using namespace rapidjson;
 
 int main(int argc, char** argv)
 {
+    uWS::Hub h;
+
+    h.onError([](void *user) {
+        switch ((long) user) {
+        case 1:
+            std::cout << "Client emitted error on invalid URI" << std::endl;
+            break;
+        case 2:
+            std::cout << "Client emitted error on resolve failure" << std::endl;
+            break;
+        case 3:
+            std::cout << "Client emitted error on connection timeout (non-SSL)" << std::endl;
+            break;
+        case 5:
+            std::cout << "Client emitted error on connection timeout (SSL)" << std::endl;
+            break;
+        case 6:
+            std::cout << "Client emitted error on HTTP response without upgrade (non-SSL)" << std::endl;
+            break;
+        case 7:
+            std::cout << "Client emitted error on HTTP response without upgrade (SSL)" << std::endl;
+            break;
+        default:
+            std::cout << "FAILURE: " << user << " should not emit error!" << std::endl;
+            exit(-1);
+        }
+    });
+ 
+
+
+    h.onConnection([](uWS::WebSocket<uWS::CLIENT> ws, uWS::HttpRequest req) {
+        uWS::OpCode opcode = uWS::OpCode::TEXT;
+        StringBuffer s;
+        Writer<StringBuffer> writer(s);
+        writer.StartObject();
+        writer.Key("command");
+        writer.String("get_current_challenge");
+        writer.Key("args");
+        writer.StartArray();
+        writer.EndArray();
+        writer.EndObject();
+        const char *payload = s.GetString();
+        int payloadlength = strlen(s.GetString());
+        switch ((long) ws.getUserData()) {
+        case 4:
+            std::cout << "Client established a remote connection over non-SSL" << std::endl;
+            ws.close(1000);
+            break;
+        case 5:
+            std::cout << "Client established a remote connection over SSL" << std::endl;
+            std::cout << "sending payload: " << s.GetString() << std::endl;
+            ws.send(payload, payloadlength, opcode);
+            //ws.close(1000);
+            break;
+        default:
+            std::cout << "FAILURE: " << ws.getUserData() << " should not connect!" << std::endl;
+            exit(-1);
+        }
+    });
+
+
+    h.onMessage([](uWS::WebSocket<uWS::CLIENT> ws, char *message, size_t length, uWS::OpCode opCode) {
+        std::cout << std::string(message, length) << std::endl;
+        solution s = solve(message);
+        printf("%d %u %s\n", s.id, s.nonce, s.hash);
+        ws.close(1000);
+    });
+
+    h.onDisconnection([](uWS::WebSocket<uWS::CLIENT> ws, int code, char *message, size_t length) {
+        std::cout << "Client got disconnected with data: " << ws.getUserData() << ", code: " << code << ", message: <" << std::string(message, length) << ">" << std::endl;
+    });
+
+    h.connect("wss://ec2-54-186-71-144.us-west-2.compute.amazonaws.com:8989/client", (void *) 5);
+    h.run();
+    /*
     (void)argc; (void)argv;
     char input[4096] = {0};
     std::cin.read(input, 4096);
     solution s = solve(input);
     printf("%d %u %s\n", s.id, s.nonce, s.hash);
+    */
 }
