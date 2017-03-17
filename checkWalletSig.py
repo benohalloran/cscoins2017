b2cece53e96deafd4227ded28a380ef4c2b0465a
Jack Valinsky <jvalinsk@u.rochester.edu>
2017-03-17 15:20:38 -0400

    forgot to push some changes to clients.cpp and checkWalletSig.py

--- checkWalletSig.py
+++ checkWalletSig.py
@@ -2,13 +2,8 @@ from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA256
 from Crypto import Random
 from Crypto.Signature import PKCS1_v1_5
-from binascii import unhexlify
 
-
-
-
-
-signature =  open('wallet.sig').read() 
+signature =  open('wallet.sig', 'rb').read() 
 
 pubkey_pem = open('rsa-public.pem').read() 
 pubkey_der_fromfile = open('rsa-public.der', 'rb').read()
@@ -16,7 +11,7 @@ pub_key = RSA.importKey(pubkey_pem)
 pub_key_der = RSA.importKey(pubkey_pem)
 pubkey_der_exported = pub_key_der.exportKey(format='DER')
 
-h = SHA256.new(pubkey_der_fromfile)
+h = SHA256.new(pubkey_der_fromfile) 
 wallet_id = h.hexdigest()
 
 print("wallet_id: " + wallet_id)
