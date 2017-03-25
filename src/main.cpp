6f8669e07400e26bd274c369491c85057154f403
Jacob Bisnett <jacob.bisnett@gmail.com>
2017-03-24 20:49:38 -0400

    change hostname

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ using namespace rapidjson;
 int main(int argc, char** argv)
 {
     cout << "Starting up client..." << endl;
-    string hostname = "ec2-54-186-71-144.us-west-2.compute.amazonaws.com";
+    string hostname = "cscoins.2017.csgames.org/client";
     int    port     = 8989;
     bool ssl = true;
     MinerClient client = MinerClient(hostname, port, ssl);
